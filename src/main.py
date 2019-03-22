6c9c54faae7ae1deb12fedc793ce10e9f03a5f08
南基雄 <kiun.namu@groove-x.com>
2019-03-22 10:54:59 +0900

    Changed directory structure and file extensions

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 import sys
 import os
 import re
-import importlib
-import solution
 
 SOLUTION_DIRECTORY = "solution"
 INPUT_FILE_DIR = "in"
@@ -20,15 +18,15 @@ def get_files(dir):
             result.append(os.path.relpath(os.path.join(cur, file), dir))
     return result
 
-def remove_extension(file):
-    pass
+def remove_extension(path):
+    return '.'.join(path.split('.')[:-1]) 
 
 def get_file_names_with_extension(extension):
     return [file for file in get_files('.') if file.endswith(f".{extension}")]
 
-def compare_two_files(file1, file2):
-    f1 = open(file1)
-    f2 = open(file2)
+def compare_two_files(path1, path2):
+    f1 = open(path1)
+    f2 = open(path2)
 
     while True:
         line1 = f1.readline()
@@ -41,30 +39,29 @@ def compare_two_files(file1, file2):
 
 cpp_files = get_file_names_with_extension("cpp")
 in_files = get_file_names_with_extension("in")
-answer_files = get_file_names_with_extension("ok")
+answer_files = get_file_names_with_extension("answer")
 
 # Solve all problems and check output
 for cpp_file in cpp_files:
-
     for in_file in in_files:
         if (TEST_MODE == 1 and "large" in in_file):
             continue
         if (TEST_MODE == 2 and "test" not in in_file):
             continue
 
-        if in_file.startswith(cpp_files):
-            handle = open(os.path.join(INPUT_FILE_DIR, in_file))
+        if in_file.startswith(remove_extension(cpp_file)):
+            handle = open(in_file)
             out_file = in_file.replace('.in', '.out')
-            out_handle = open(os.path.join(OUTPUT_FILE_DIR, out_file), 'w')
-            eval("{}.solve({}, {})".format(module, 'handle', 'out_handle'))
+            out_handle = open(out_file, 'w')
             handle.close()
             out_handle.close()
 
             # Compare with already proven answers
-            if (out_file not in answer_files):
-                print("Not solved:\t\t{}".format(out_file))
-            elif not compare_two_files(os.path.join(OUTPUT_FILE_DIR, out_file), os.path.join(ANSWER_FILE_DIR, out_file)):
-                    print("Output error:\t\t{}".format(out_file))
+            answer_file_to_be = in_file.replace('.in', '.answer')
+            if (answer_file_to_be not in answer_files):
+                print("Not solved:\t\t{}".format(in_file))
+            elif not compare_two_files(out_file, answer_file_to_be):
+                print("Output error:\t\t{}".format(out_file))
             else:
                 print("Correct: {}".format(out_file))
 
